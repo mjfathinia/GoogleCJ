aa477d2a5f221455ca91d5988aad4a64796a2912
Mohammad J. Fathinia <mj.fathinia@gmail.com>
2011-10-02 05:47:48 +0400

    Added Euro Python

---
+++ 2011EuroPython/a.py
@@ -0,0 +1,32 @@
+import sys
+import re
+
+def findRuler(country):
+    lastChar = country.lower()[-1]
+    if lastChar == 'y':
+        return 'nobody'
+    elif lastChar in ['a','e','i','o','u']:
+        return 'a queen'
+    else:
+        return 'a king'
+
+def main():
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print 'Usage: a.py input output'
+        sys.exit(1)
+    inputFile = open(args[0],'rU')
+    result = []
+    T = int(inputFile.readline().strip())
+    for t in range(T):
+        country = inputFile.readline().strip()
+        result.append('Case #%d: %s is ruled by %s.\n' % (t+1,country,findRuler(country)))
+    inputFile.close()
+    outputFile = open(args[1], 'w')
+    outputFile.writelines(result)
+    outputFile.close()
+
+if __name__ == '__main__':
+    main()
+
+
